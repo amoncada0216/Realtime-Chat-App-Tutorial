5c0c22e7bc58db59421cf7766988bbc93419f805
Abraham Moncada <a.moncada0216@gmail.com>
2026-01-29 18:08:47 -0500

    message routes added

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,16 @@
-import express from "express"
+import { getAllContacts, getChatPartners, getMessagesByUserId, sendMessage } from "../controllers/message.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
+import { arcjetProtection } from "../middlewares/arcjet.middleware.js"
 
-const router = express.Router()
+import express from "express";
 
-router.get("/send", (req, res) => {
-    res.send("Send message endpoint.")
-})
+const router = express.Router();
 
-export default router
+router.use(arcjetProtection, protectRoute)
+
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners)
+router.get("/:id", getMessagesByUserId)
+router.post("/send/:id", sendMessage)
+
+export default router;
