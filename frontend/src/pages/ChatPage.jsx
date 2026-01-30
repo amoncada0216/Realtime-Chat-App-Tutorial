42b4788c72ad5e5b14e7bc19e69910fc14820473
Abraham Moncada <a.moncada0216@gmail.com>
2026-01-30 13:48:12 -0500

    sidebar completed

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -17,6 +17,7 @@ function ChatPage() {
         {/* LEFT SIDE */}
         <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col">
           <ProfileHeader />
+
           <ActiveTabSwitch />
 
           <div className="flex-1 overflow-y-auto p-4 space-y-2">
